359d2a00fd1d14e3f06e73b77c382ee0ea8f7bee
Miguel <protoblae@gmail.com>
2022-02-08 12:37:17 +0900

    Testing Pycharm

---
+++ main.py
@@ -0,0 +1,12 @@
+import turtle
+
+Roshi = turtle.Turtle()
+Roshi.shape("turtle")
+Roshi.color("green")
+Roshi.forward(100)
+
+
+myScreen = turtle.Screen()
+# myScreen.screensize(1280, 720)
+
+myScreen.exitonclick()
